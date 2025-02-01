76c4bd1c9e54a20b3430c47bbee97b5497610e20
Eduardo Melo <eduardoddmg@gmail.com>
2025-02-01 11:13:57 -0300

    [FEAT] Início da implementação do form profile

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,21 +1,29 @@
+'use client';
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { useAuth } from '@/context/auth-context';
+import { ProfileForm } from '@/modules/profile/form';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 const Page = () => {
+  const { token, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!token && !loading) router.push('/login');
+  }, [token, loading]);
+
+  const onSubmit = (data) => console.log(data);
   return (
     <div className="space-y-5">
       <h1>Perfil</h1>
-      <Tabs defaultValue="account">
+      <Tabs defaultValue="dados-pessoais">
         <TabsList>
           <TabsTrigger value="dados-pessoais">Dados pessoais</TabsTrigger>
-          <TabsTrigger value="seguranca-privacidade">
-            Segurança e privacidade
-          </TabsTrigger>
         </TabsList>
         <TabsContent value="dados-pessoais">
-          Corrija os seus dados pessoais aqui.
-        </TabsContent>
-        <TabsContent value="seguranca-privacidade">
-          Mude suas informações confidenciais.
+          <ProfileForm onSubmit={onSubmit} />
         </TabsContent>
       </Tabs>
     </div>
---
+++ frontend/modules/profile/form.tsx
@@ -0,0 +1,188 @@
+'use client';
+
+import * as React from 'react';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+
+const FormSchema = z.object({
+  name: z.string().min(1, 'O nome é obrigatório'),
+  email: z.string().email('E-mail inválido'),
+  phone: z.string().min(8, 'Número de telefone inválido'),
+  address: z.string().min(5, 'Endereço obrigatório'),
+  city: z.string().min(1, 'Cidade obrigatória'),
+  state: z.string().min(1, 'Estado obrigatório'),
+  country: z.string().min(1, 'País obrigatório'),
+  zipCode: z.string().min(5, 'CEP obrigatório'),
+  birthDate: z.string().min(1, 'Data de nascimento obrigatória'),
+  occupation: z.string().min(1, 'Ocupação obrigatória'),
+});
+
+interface ProfileFormProps {
+  onSubmit: (data: z.infer<typeof FormSchema>) => void;
+  defaultValues?: Partial<z.infer<typeof FormSchema>>;
+}
+
+export const ProfileForm: React.FC<ProfileFormProps> = ({
+  onSubmit,
+  defaultValues = {},
+}) => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues,
+  });
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nome</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="phone"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Telefone</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="address"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Endereço</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="city"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Cidade</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="state"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Estado</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="country"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>País</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="zipCode"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>CEP</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="birthDate"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Data de Nascimento</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="occupation"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Ocupação</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          isLoading={form.formState.isSubmitting}
+          variant="default"
+        >
+          Salvar Alterações
+        </Button>
+      </form>
+    </Form>
+  );
+};
